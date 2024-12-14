224f436e23b5d519affb772e96ba95cdcc59cdc5
Tom <tom@vespertool.com>
2024-12-14 18:01:20 +0100

    13a

---
+++ 13a.js
@@ -0,0 +1,34 @@
+import { readInput} from "./lib.js";
+
+const input = readInput(11);
+const machines = []
+
+for (let i = 0; i < input.length; i += 4) {
+    const a = input[i].match(/(\d+)/g);
+    const b = input[i + 1].match(/(\d+)/g);
+    const prize = input[i + 2].match(/(\d+)/g);
+
+    machines.push({
+        a,
+        b,
+        prize
+    })
+}
+
+const prizes = [];
+
+for (const {a, b, prize} of machines) {
+    console.log(prize);
+    for (let aPress = 0; aPress < 100; aPress++) {
+        for (let bPress = 0; bPress < 100; bPress++) {
+            const x = a[0] * aPress + b[0] * bPress;
+            const y = a[1] * aPress + b[1] * bPress;
+
+            if (x == prize[0] && y == prize[1]) {
+                prizes.push({ aPress, bPress, prize, tokens: aPress * 3 + bPress })
+            }
+        }
+    }
+}
+
+console.log(prizes.reduce((sum, { tokens }) => sum + tokens, 0));
